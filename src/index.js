2d276b91a65d74e5091577e0f37a1a76358f7b76
Pavel Zhdanovich <zhdan21@gmail.com>
2020-05-19 22:11:04 +0300

    changes 190520

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
+/* eslint-disable max-len */
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable consistent-return */
 export default class Settings {
-  constructor() {
+  constructor(...rest) {
     this.defaultOptions = new Map([['theme', 'dark'], ['music', 'trance'], ['difficulty', 'easy']]);
     this.allOptions = [{ theme: ['dark', 'light', 'gray'] }, { music: ['trance', 'pop', 'rock', 'chillout', 'off'] }, { difficulty: ['easy', 'hard', 'nightmare'] }];
-    this.userOptions = new Map(this.defaultOptions);
-  }
-
-  changeSettings(option, choise) {
-    const search = this.allOptions.find((element) => element.hasOwnProperty(option));
-    if (search === undefined) {
-      return Error('Такой настройки не существует');
-    }
-    const selection = search[option];
-    if (selection.includes(choise) === false) {
-      return Error('Такой опции не существует');
-    }
-    this.userOptions.set(option, choise);
+    this.userOptions = new Map();
+    this.enterUserOption = [...rest].forEach((element) => this.userOptions.set(element[0], element[1]));
   }
 
   get settings() {
-    return this.userOptions;
+    const returnOptions = this.defaultOptions;
+    this.userOptions.forEach((value, key) => {
+      returnOptions.set(key, value);
+    });
+    return returnOptions;
   }
 }
